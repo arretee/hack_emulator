32078b55246db4f52a37760a10f6ecd6304b1cbc
arretee <arreteear@gmail.com>
2026-08-31 16:42:52 +0300

    added gui_config.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,11 @@
-from src.hack_computer import HackComputer
-from src.hack_config import * 
-from src.binary_functions import * 
-
-from time import sleep
-
-hack_computer = HackComputer()
-hack_computer.load_instructions("hack_codes/Mult.hack")
-
-hack_computer.RAM[R0] = 8
-hack_computer.RAM[R1] = 10
+class HackEmulator:
+    def __init__(self):
+        pass
 
 
-while True:
-    hack_computer.execute_command()
+    def run(self):
+        pass
 
-    for i in range(16):
-        print(f"R{i}: ", hack_computer.RAM[i])
-
-
-
+if __name__ == "__main__":
+    hack_emulator = HackEmulator()
+    hack_emulator.run()
